519f2f4de58dea04dd1cec8a8fd0b09f67b12d35
shahoul95 <shahoul@libheros.fr>
2025-02-19 10:16:39 +0100

    add commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Register from './components/Register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
+import LogoutButton from "./components/LogoutButton";
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl">Bienvenue</h1>
+      <LogoutButton />
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
